f180d129c53e31121c1d4b64b27918b1c07d611e
abdul-albinali-2009 <abdulrahman.albinali@stu.ocsb.ca>
2024-12-05 11:03:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from random import *
+#computer picks a random number between 1 and 6
 computerNumber = randint(1, 6)
+#asks user to pick number between 1 and 6
 userNumber = int(input("Guess a random number between 1 and 6: "))
+#if the user number is equal to the computer number it will print you win, else you wrong
 if userNumber == computerNumber:
   print("You guessed right")
 else:
